2836d504879a5e7e2bf76e24f73d4b8d2d300509
Adam Kalinowski <Adam>
2023-11-21 12:44:15 -0500

    FEAT: textarea component added

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,9 @@
-// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
-
 /** @type {import("prettier").Config} */
 const config = {
-  trailingComma: "es5",
-  tabWidth: 4,
-  semi: false,
-  singleQuote: true,
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
 };
 
 export default config;
